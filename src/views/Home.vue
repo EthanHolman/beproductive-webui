8b4dfce80edbbca80077a4cc6c5522b468eaeaf7
Ethan Holman <96ethanh@gmail.com>
2021-12-25 17:53:53 -0800

    Saving to localStorage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,7 @@
     <accomplishment-editor v-model="accomplishments" />
     <did-not-go-well-editor v-model="didNotGoWell" />
   </div>
+  <button @click="save" class="saveButton">Save!</button>
 </template>
 
 <script lang="ts">
@@ -12,25 +13,41 @@ import { defineComponent } from "vue";
 import DayRater from "@/components/DayRater.vue";
 import AccomplishmentEditor from "@/components/AccomplishmentEditor.vue";
 import DidNotGoWellEditor from "@/components/DidNotGoWellEditor.vue";
+import { LOCAL_STORAGE_KEY } from "@/globals";
+
+const formDataModel = {
+  rating: 0,
+  todaysDate: new Date(),
+  accomplishments: [
+    "did laundry",
+    "robbed a bank",
+    "venmo'd my buddy bail money",
+  ],
+  didNotGoWell: ["went to jail"],
+};
 
 export default defineComponent({
   name: "Home",
   data() {
-    return {
-      rating: 0,
-      todaysDate: new Date(),
-      accomplishments: [
-        "did laundry",
-        "robbed a bank",
-        "venmo'd my buddy bail money",
-      ],
-      didNotGoWell: ["went to jail"],
-    };
+    return { ...formDataModel };
   },
   components: { DayRater, AccomplishmentEditor, DidNotGoWellEditor },
   methods: {
     updateValue(newValue: number) {
       this.rating = newValue;
+    },
+    save() {
+      const formData = { ...this.$data };
+      const pastRecords = JSON.parse(
+        localStorage.getItem(LOCAL_STORAGE_KEY) ?? "[]"
+      );
+      localStorage.setItem(
+        LOCAL_STORAGE_KEY,
+        JSON.stringify([...pastRecords, formData])
+      );
+
+      Object.assign(this.$data, { ...formDataModel });
+      // this.$data = { ...formDataModel };
     },
   },
 });
@@ -44,5 +61,10 @@ export default defineComponent({
 .editorsContainer {
   margin: auto;
   width: 80%;
+}
+
+.save {
+  margin: auto;
+  display: block;
 }
 </style>
